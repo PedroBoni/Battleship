76c7c190829216ea4abdb9c722cffc0b18fa5dc6
Pedro Henrique Bonifácio Martins <pedro.henrique1210@outlook.com>
2019-11-18 10:56:21 -0300

    Secundary commit

--- code.js
+++ code.js
@@ -1,6 +1,8 @@
-var location1 = 3
-var location2 = 4
-var location3 = 5
+var randomLoc = Math.floor(Math.random() * 5)
+
+var location1 = randomLoc
+var location2 = location1 + 1
+var location3 = location2 + 1
 
 var guess;
 var hits = 0
@@ -17,6 +19,10 @@ while(isSunk == false){
       console.log(guesses)
    }
    if(guess == location1 || guess == location2 || guess == location3) {
+      if(guess ==location1){location1 = null}
+      if(guess ==location2){location2 = null}
+      if(guess ==location3){location3 = null}
+               
       hits = hits + 1 
 
       alert("acertou")
